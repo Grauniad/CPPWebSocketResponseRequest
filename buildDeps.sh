c09670e037334f487c737c163b73b6186817623d
Luke Humphreys <lhumphreys67@gmail.com>
2018-03-25 17:33:34 +0100

    Configure the build-dependencies / travis scripts and suppress valgrind issues
    A couple of issues are showing up on valgrind. Supress them for now to
    get the build going.

--- buildDeps.sh
+++ buildDeps.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-DEPS_BUILD=$PWD/deps/build
-if [[ "$1" == "" ]]; then
-    echo "No install directory provided, falling back to: $DEPS_BUILD"
-else
-    DEPS_BUILD=$1
-fi
-
-DEPS_CMAKE_DEPO=$PWD/deps/build/lib/cmake
-mkdir -p $DEPS_BUILD
-
 if [[ -e CPPWebSocketResponseRequestConfig.cmake ]]; then
     echo "Building dependencies..."
 else
@@ -17,37 +7,21 @@ else
     exit 1
 fi
 
-if [[ -e deps/FixedJSONParserCPP ]]; then
-    echo "Existing FixedJSONParserCPP directory, no need to clone"
+if [[ "$1" == "" ]]; then
+    DEPS_ROOT="$PWD/deps"
 else
-    git clone https://github.com/Grauniad/FixedJSONParserCPP.git deps/FixedJSONParserCPP || exit 1
+    DEPS_ROOT="$1"
+fi
+if [[ -e $DEPS_ROOT/CMakeUtils ]]; then
+    echo "Existing CMakeUtils directory, no need to clone"
+else
+    git clone https://github.com/Grauniad/CMakeUtils.git $DEPS_ROOT/CMakeUtils || exit 1
 fi
 
-if [[ -e deps/websocketpp ]]; then
-    echo "Existing websocketpp directory, no need to clone"
-else
-    git clone https://github.com/zaphoyd/websocketpp.git deps/websocketpp || exit 1
-fi
+declare -A depList
 
+depList[JSON]=https://github.com/Grauniad/FixedJSONParserCPP.git
+depList[websocketpp]=https://github.com/zaphoyd/websocketpp.git
+depList[Threads]=https://github.com/Grauniad/CPPThreadUtils.git
 
-
-deps=(FixedJSONParserCPP);
-for dep in ${deps[@]} ; do
-    mkdir -p deps/$dep/build
-
-    pushd deps/$dep || exit 1
-
-    if [[ -e buildDeps.sh ]]; then
-        ./buildDeps.sh $DEPS_BUILD || exit 1
-    fi
-    git pull
-    pushd build || exit 1
-
-    cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_PREFIX_PATH:PATH=$DEPS_CMAKE_DEPO" "-DCMAKE_INSTALL_PREFIX:PATH=$DEPS_BUILD" .. || exit 1
-    make -j 3 || exit 1
-    make install || exit 1
-
-    popd || exit 1
-    popd || exit 1
-    done
-exit 1
+source $DEPS_ROOT/CMakeUtils/build_tools/buildDepsCommon.sh || exit 1
